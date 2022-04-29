b10261f05279056dda999dd367b7a69912553718
Shabdar <shabdar.ghata@gmail.com>
2022-04-29 10:02:57 -0400

    added pandas

--- 10_pandas.py
+++ 10_pandas.py
@@ -12,3 +12,23 @@ print (cars)
 
 mycars = pd.DataFrame(cars)
 print(mycars)
+print(pd.__version__)
+
+#What is a Series?
+#A Pandas Series is like a column in a table.
+#It is a one-dimensional array holding data of any type.
+
+a= [1,5,7]
+aseries = pd.Series(a)
+print(aseries)
+
+#Label a series
+b = [7,8,9]
+bseries = pd.Series(b, index = ["x","y","z"])
+print(bseries)
+#Return the value of "y":
+print(bseries["y"])
+
+#Read csv file
+df = pd.read_csv("csv/aes_pv.csv")
+print(df)
