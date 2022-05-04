681f667d04aff3cea6fdf61d6e6be2b2ff4494e2
Shabdar <shabdar.ghata@gmail.com>
2022-05-04 12:46:55 -0400

    excel write

--- 11_excel_read_write.py
+++ 11_excel_read_write.py
@@ -4,4 +4,9 @@ df = pd.read_excel("excel/SampleData.xlsx",sheet_name="SalesOrders")
 print(df)
 
 #Locate a specific row
-print(df.loc[[0,1,3]])
+print(df.loc[[0,1,2]])
+
+#Write excel file
+with pd.ExcelWriter("excel/SampleData.xlsx",mode="a",if_sheet_exists="replace") as writer:
+    df.loc[[0,1,2]].to_excel(writer,sheet_name="SalesOrdersFirstThree")
+
